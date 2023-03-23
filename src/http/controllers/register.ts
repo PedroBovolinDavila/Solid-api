bedec7752ed86d7b29a012de75bd4c25293cb625
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-03-23 18:49:58 -0300

    add error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlredyExistsError } from '@/use-cases/errors/user-alredy-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,7 +23,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlredyExistsError) {
+      return reply.status(409).send({
+        message: err.message,
+      })
+    }
+
+    throw err
   }
 
   return reply.status(201).send()
