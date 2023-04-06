29ac328f296249ee94a3e40f93009196574c65ca
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-04-05 22:52:17 -0300

    check ins controllers

--- src/use-cases/factories/make-get-user-metrics-use-case.ts
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -1,7 +1,7 @@
 import { GetUserMetricsUseCase } from '../get-user-metrics'
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeUserMetricsUseCase() {
+export function makeGetUserMetricsUseCase() {
   const prismaCheckInsRepository = new PrismaCheckInsRepository()
   const useCase = new GetUserMetricsUseCase(prismaCheckInsRepository)
 
